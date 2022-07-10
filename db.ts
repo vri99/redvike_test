70e482ebec0a76e3364a23b6908c1ac04e668e08
rostyslavvoloshyn <styslav.v@gmail.com>
2022-07-10 09:47:25 +0300

    refactor db connection

--- db.ts
+++ db.ts
@@ -1,10 +1,10 @@
 import {DataSource} from "typeorm";
 
 const dataSource: DataSource = new DataSource({
-    database: "postgres",
-    password: "password",
-    username: "postgres",
-    host: "postgres",
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    username: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
     port: 5432,
 
     entities: [process.env.TYPEORM_ENTITIES],
